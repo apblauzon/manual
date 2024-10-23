b2f290c519e1e6d15e579f8276254db93598fd29
apblauzon <apblauzon@gmail.com>
2024-10-23 08:55:23 +0800

    Update 01 Upload Data.py
    Change #1

--- pages/01 Upload Data.py
+++ pages/01 Upload Data.py
@@ -5,7 +5,7 @@ import pandas as pd
 def show_uploading_data():
     st.header("Uploading Your Data File")
     st.markdown("""
-    1. **Ensure Your Data is Clean**: Remove missing values, typographical errors, and superfluous characters. Address outliers outside of DatViz AI, if necessary, then re-upload your data.
+    1. First and foremost, if you have **sensitive information** in your dataset, we  suggest either deleting that column or using some ID that will camouflage the data, but stilll make sense to your data visualization and analysis. 
     2. **Use Only One Header**: Your data should have a single, clear header.
     3. **Column and Row Organization**: Columns represent variables, and rows represent observations.
     4. **Name Columns Properly**: Use short, meaningful names for each column.
