9b50beaf2d2e5f07d3e04d4f76ec755bcfaad032
apblauzon <apblauzon@gmail.com>
2024-11-02 09:39:09 +0800

    Add files via upload

--- Introduction.py
+++ Introduction.py
@@ -9,13 +9,15 @@ def show_intro():
     st.write("")
     # Display the header and text
     st.markdown("""
-    **Welcome to DatViz AI**, a data analytics application powered by OpenAI model ChatGPT4o. Using natural conversation, you can ask DatViz AI to simplify or summarize your data, calculate summary statistics, and generate clear and intuitive visualizations. It can be used in any field requiring quick data analysis and visualization.  DatViz Ai can also be integrated into decision-making and planning processes in business, government, not-for-profit, education and research institutions.
-    
-    To use DatViz AI effectively, you should be familiar with your data, understand the context in which it was collected, and have some basic understanding of statistics. It is also important to have clear goals for analyzing your data.
+    **Welcome to DatViz AI**, a data analytics application powered by OpenAI model ChatGPT4o. Using natural conversation, you can ask DatViz AI to simplify or summarize your data, calculate summary statistics, and generate clear and intuitive visualizations. It can be used in any field requiring quick data analysis and visualization. DatViz Ai can also be integrated into decision-making and planning processes in business, government, not-for-profit, education and research institutions.
+
+For the initial launch, users are given free 4,000 credits (or tokens) with additional 2,000 if you fill in our user experience survey. The number of credits used vary depending on the number of words used in prompts and length of output.
+
+To use DatViz AI effectively, you should be familiar with your data, understand the context in which it was collected, and have some basic understanding of statistics. It is also important to have clear goals for analyzing your data.
+
     """)
 
-    st.write("")
-    st.write('Source of data of this demo: Retail_Transaction_Dataset.csv (from Kaggle) but with mocked-up locations and added a column "Marketing Promotions" ')
+    st.write('Source of data of this demo: Retail_Transaction_Dataset.csv (from Kaggle) but with mocked-up locations and added a column "Marketing Promotions"')
 
 
 show_intro()
